ac77c2acb66380e28215d72a71bb1f0e29042a9c
Daniel Hawkins <90428134+NomadDsgns@users.noreply.github.com>
2021-09-11 22:35:16 -0500

    Update NomadButton.js

--- NomadButton.js
+++ NomadButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, View, StyleSheet } from 'react-native';
 
-export default function NomadButton({ title, onPress }) {
+export default function({ title, onPress }) {
     return (
         <View style={styles.container}>
             <Button style={styles.button}
