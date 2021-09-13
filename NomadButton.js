3b9102a5c470778ff77ad9cb4d6829ce242d0764
Daniel Hawkins <90428134+NomadDsgns@users.noreply.github.com>
2021-09-12 23:23:55 -0500

    Fixed code

--- NomadButton.js
+++ NomadButton.js
@@ -1,31 +1,37 @@
 import React from 'react';
-import { Button, View, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
 
 export default function({ title, onPress }) {
     return (
         <View style={styles.container}>
-            <Button style={styles.button}
-                    onPress={onPress}>{title}</Button>
+            <TouchableOpacity style={styles.button}
+                              onPress={onPress}>
+              <Text style={styles.text}>{title}</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-
-    },
-    button: {
-        backgroundColor: "#ffec64",
-        borderRadius: 27,
-        borderWidth: 1,  
-        borderColor: "#ffaa22",
-        color:  "#333",
-        fontSize: 14,
-        fontWeight: "bold",
-        paddingTop: 6,
-        paddingBottom: 6,
-        paddingLeft: 24,
-        paddingRight: 24
-    }
+  container: {
+    width: 250
+  },
+  button: {
+    backgroundColor: "#474747",
+    borderRadius: 10,
+    borderWidth: 1,  
+    borderColor: "#757575",
+    fontSize: 14,
+    fontWeight: "bold",
+    paddingTop: 6,
+    paddingBottom: 6,
+  },
+  text: {
+    textAlign: "center",
+    color:  "white",
+    fontWeight: "bold",
+    paddingTop: 5,
+    paddingBottom: 5
+  }
 })
